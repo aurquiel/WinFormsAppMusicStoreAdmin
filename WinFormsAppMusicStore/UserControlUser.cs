0c72c5cfab208904ae46592e197233ca4af97c34
egomez <egomez@gt99.com.ve>
2023-07-19 16:03:18 -0400

    visual stuido file

--- WinFormsAppMusicStore/UserControlUser.cs
+++ WinFormsAppMusicStore/UserControlUser.cs
@@ -1,14 +1,6 @@
 ﻿using ClassLibraryModels;
 using ClassLibraryServices;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace WinFormsAppMusicStoreAdmin
 {
